10a29e4eb2846d52dd0c32c1ddd02087b5553097
kwonsejin <kwon_sejin@naver.com>
2021-01-30 01:30:42 +0900

    feat(appbar) : design initial appbar with temporary logo

--- frontend/src/components/core/AppBar.vue
+++ frontend/src/components/core/AppBar.vue
@@ -1,10 +1,51 @@
 <template>
-  <h2>This is AppBar</h2>
+  <v-card
+    class="mx-auto"
+    width="100%"
+    flat
+  >
+    <!-- status bar -->
+    <v-card-text
+      style="text-align: right; color: black; background-color: #ECECEC"
+      class="pa-2"
+    >
+      <a href="#" class="mx-1" style="color: black; text-decoration:none; word-break: keep-all">로그인</a>
+      <a href="#" class="mx-1" style="color: black; text-decoration:none; word-break: keep-all">회원가입</a>
+      <a href="#" class="mx-1" style="color: black; text-decoration:none; word-break: keep-all">고객센터</a>
+    </v-card-text>
+
+    <!-- main appbar -->
+    <v-card-text
+      class="pa-0"
+      style="display: flex;"
+    >
+      <!-- Categories -->
+      <Categories />
+
+      <!-- logo(temporary) -->
+      <v-img
+        src="@/assets/temporary_logo.png"
+        class="my-auto mx-5"
+        max-height="80"
+        max-width="187"
+      >
+      </v-img>
+
+      <!-- SearchBar -->
+      <SearchBar class="my-auto" />
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import Categories from './AppBar/Categories.vue'
+import SearchBar from './AppBar/SearchBar.vue'
 export default {
   name: 'AppBar',
+  components: {
+    Categories,
+    SearchBar,
+  },
 }
 </script>
 
---
+++ frontend/src/components/core/AppBar/SearchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="display: flex; border: 3px skyblue solid">
+    <v-select
+      v-model="e1"
+      :items="items"
+      solo
+      flat
+      
+      style="max-width: 130px"
+    ></v-select>
+
+    <v-text-field
+      
+      placeholder="찾고 싶은 상품을 검색해보세요!"
+      clearable
+      solo
+      flat
+      style="width: 300px"
+    ></v-text-field>
+    <v-icon large>
+      mdi-magnify
+    </v-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data: () => ({
+      e1: '전체',
+      items: ['전체', '여성패션', '남성패션', '뷰티'],
+    }),
+}
+</script>
+
+<style scoped>
+  >>> .v-text-field__details {
+    display: none;
+  }
+
+  >>> .v-input__slot {
+    margin: 0;
+  }
+</style>
