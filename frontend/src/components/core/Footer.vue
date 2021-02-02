d80acf3232b0d09f8b17f53f530b812a626b7ff1
kwonsejin <kwon_sejin@naver.com>
2021-02-02 15:21:42 +0900

    feat(footer) : footer with lorem ipsum

--- frontend/src/components/core/Footer.vue
+++ frontend/src/components/core/Footer.vue
@@ -36,8 +36,23 @@
           >
           </v-img>
         </v-col>
-        <v-col cols="3">Team 중대장은 만족했다</v-col>
-        <v-col cols="3">^-^</v-col>
+        <v-col cols="4">
+          <div style="font-size: 15px;">
+            
+            EUREKA 유레카 | TEAM 중대장은 만족했다
+            <br>
+            경상북도 구미시 진평4길 21
+            <br>
+            등록번호 : 000-00-00000
+            <br>
+            전화번호 : 000-0000-0000
+          </div>
+        </v-col>
+        <v-col cols="2">
+          <div style="word-break: break-all; font-size: 15px;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          </div>
+        </v-col>
       </v-row>
 
       <v-col
@@ -45,7 +60,7 @@
         cols="12"
       >
         <v-card-text class="py-2 text-center" style="color: white; width=100%; background-color: #333333;">
-          Copyright © <strong>EUREKA</strong> Corp. 2021-{{ new Date().getFullYear() }} All Rights Reserved.
+          Copyright © <strong>EUREKA</strong> Team. 2021-{{ new Date().getFullYear() }} All Rights Reserved.
         </v-card-text>
       </v-col>
     </v-row>
