1f8b3dcd00b92df0560182b2fdd997172e48aca7
csy1874 <csy1874@naver.com>
2021-02-05 06:48:00 +0900

    changed

--- backend/Seller/src/main/java/com/eureka/seller/controller/SellerController.java
+++ backend/Seller/src/main/java/com/eureka/seller/controller/SellerController.java
@@ -2,10 +2,9 @@ package com.eureka.seller.controller;
 
 import com.eureka.seller.entity.Seller;
 import com.eureka.seller.service.SellerService;
+import org.hibernate.usertype.UserVersionType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,31 @@ public class SellerController {
 
     @PostMapping("/addSellers")
     public List<Seller> addSellers(@RequestBody List<Seller> sellers){
+        return service.saveSellers(sellers);
+    }
 
+    @GetMapping("/sellers")
+    public List<Seller> findAllSellers(){
+        return service.getSellers();
+    }
+
+    @GetMapping("/sellerById/{id}")
+    public Seller findSellerById(@PathVariable int id){
+        return service.getSellerById(id);
+    }
+
+    @GetMapping("/seller/{name}")
+    public Seller findSellerByName(@PathVariable String name){
+        return service.getSellerByName(name);
+    }
+
+    @PutMapping("/updateSeller")
+    public Seller updateSeller(@RequestBody Seller seller){
+        return service.updateSeller(seller);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteSeller(@PathVariable int id){
+        return service.deleteSeller(id);
     }
 }
