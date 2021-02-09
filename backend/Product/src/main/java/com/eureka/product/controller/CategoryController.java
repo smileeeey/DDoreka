76e62c799711a82c2163543d1681442899f34125
sumin416 <sumin416@naver.com>
2021-02-09 23:54:59 +0900

    Change Product Server

--- backend/Product/src/main/java/com/eureka/product/controller/CategoryController.java
+++ backend/Product/src/main/java/com/eureka/product/controller/CategoryController.java
@@ -12,17 +12,17 @@ public class CategoryController {
 
     private final CategoryService service;
 
-    public CategoryController(CategoryService service){
+    public CategoryController(CategoryService service) {
         this.service = service;
     }
 
     // main 화면 카테고리 정보 가져오기
     @GetMapping("/mainCategory")
-    public Response findMainCategories(){
+    public Response findMainCategories() {
         Response response;
-        try{
+        try {
             response = new Response("success", "메인 카테고리 조회 성공", service.getMainCategories());
-        }catch(Exception e){
+        } catch (Exception e) {
             response = new Response("error", "메인 카테고리 조회 실패", e.getMessage());
         }
 
@@ -31,22 +31,22 @@ public class CategoryController {
 
     // depth=3인 카테고리 id로 하위 카테고리 정보 가져오기
     @GetMapping("/subCategory/{categoryId}")
-    public Response findSubCategories(@PathVariable String categoryId){
+    public Response findSubCategories(@PathVariable String categoryId) {
         Response response;
-        try{
+        try {
             response = new Response("success", "하위 카테고리 조회 성공", service.getSubCategories(categoryId));
-        }catch(Exception e){
+        } catch (Exception e) {
             response = new Response("error", "하위 카테고리 조회 실패", e.getMessage());
         }
         return response;
     }
 
     @PostMapping("/add")
-    public Response addCategory(@RequestBody Category category){
+    public Response addCategory(@RequestBody Category category) {
         Response response;
-        try{
+        try {
             response = new Response("success", "카테고리 등록 성공", service.saveCategory(category));
-        }catch(Exception e){
+        } catch (Exception e) {
             response = new Response("error", "카테고리 등록 실패", e.getMessage());
         }
 
--- backend/Product/src/main/java/com/eureka/product/service/ProductService.java
+++ backend/Product/src/main/java/com/eureka/product/service/ProductService.java
@@ -31,8 +31,8 @@ public class ProductService {
     }
 
     //카테고리별로 상품 정보 가져오기
-    public List<Product> getProductsByCategory(String categoryId, int depth){
-        switch(depth){
+    public List<Product> getProductsByCategory(String categoryId, int depth) {
+        switch (depth) {
             case 3:
                 return productRepository.findByCategory3Id(categoryId);
             case 4:
@@ -48,47 +48,47 @@ public class ProductService {
 
         Product product = productRepository.findById(productId).orElse(null);
 
-       return product;
+        return product;
     }
 
     // 키워드로 상품 검색
-    public List<Product> getProductsByName(String category1Id,String keyword){
-        System.out.println(":::::"+category1Id);
-        System.out.println(":::::"+keyword);
+    public List<Product> getProductsByName(String category1Id, String keyword) {
+        System.out.println(":::::" + category1Id);
+        System.out.println(":::::" + keyword);
         //상품 전체에서 키워드로 상품 검색
-        if(category1Id.equals("ALL"))
+        if (category1Id.equals("ALL"))
             return productRepository.findByNameContains(keyword);
 
-        //depth=1인 카테고리 안에서 키워드로 상품 검색
+            //depth=1인 카테고리 안에서 키워드로 상품 검색
         else
-            return productRepository.findByCategory1IdAndNameContains(category1Id,keyword);
+            return productRepository.findByCategory1IdAndNameContains(category1Id, keyword);
     }
 
     //상품 정보 입력하기
-    public void saveProduct(Map<String,Object> param){
+    public void saveProduct(Map<String, Object> param) {
 
         Gson gson = new Gson();
 
         JsonParser jsonParser = new JsonParser();
 
-        Product product = gson.fromJson(param.get("product").toString(),Product.class);
+        Product product = gson.fromJson(param.get("product").toString(), Product.class);
 
         Date cur = new Date();
         product.setRegisterDate(cur);
         product.setUpdateDate(cur);
         Product productResult = productRepository.save(product);
-        System.out.println("상품 저장 완료. 아이디: "+productResult.getId());
+        System.out.println("상품 저장 완료. 아이디: " + productResult.getId());
 
-        Productoption[] optionArr = gson.fromJson(param.get("option").toString(),Productoption[].class);
+        Productoption[] optionArr = gson.fromJson(param.get("option").toString(), Productoption[].class);
         List<Productoption> options = Arrays.asList(optionArr);
 
-        Productimage[] imageArr = gson.fromJson(param.get("image").toString(),Productimage[].class);
+        Productimage[] imageArr = gson.fromJson(param.get("image").toString(), Productimage[].class);
         List<Productimage> images = Arrays.asList(imageArr);
 
-        for (int i = 0 ; i < options.size() ; ++i){
+        for (int i = 0; i < options.size(); ++i) {
             options.get(i).setProduct(productResult);
         }
-        for (int i = 0 ; i < images.size() ; ++i){
+        for (int i = 0; i < images.size(); ++i) {
             images.get(i).setProduct(productResult);
         }
 
@@ -97,7 +97,7 @@ public class ProductService {
     }
 
     //상품 정보 수정하기
-    public Product updateProduct(Product product){
+    public Product updateProduct(Product product) {
         Product existingProduct = productRepository.findById(product.getId()).orElse(null);
         existingProduct.setName(product.getName());
         existingProduct.setCategory1Id(product.getCategory1Id());
@@ -115,7 +115,7 @@ public class ProductService {
     // [[[ issue1. ]]] fk 관계인 테이블들 어떻게 삭제?
     //  방법1.update cascade 처리
     //  방법2.다른 테이블들 delete 하고 난 뒤 처리
-    public void deleteProduct(int productId){
+    public void deleteProduct(int productId) {
 
 
         productRepository.deleteById(productId);
