928a0e7c802fc52003a01daa817632d04b08404c
csy1874 <csy1874@naver.com>
2021-02-10 08:39:53 +0900

    application.yml is added

---
+++ backend/service-registry/src/test/java/com/eureka/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
