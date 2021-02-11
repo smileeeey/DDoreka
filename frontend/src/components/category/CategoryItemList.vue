3c1d9d5112213ce7da54b0a47b121c7700c5627c
anseik <kma9271@naver.com>
2021-02-12 01:59:27 +0900

    feat: connect FE-BE product

--- frontend/src/components/category/CategoryItemList.vue
+++ frontend/src/components/category/CategoryItemList.vue
@@ -3,12 +3,12 @@
     <v-container>
       <v-row>
         <v-col
-          v-for="(categoryItem, idx) in categoryItems"
+          v-for="(item, idx) in items"
           :key="idx"
           class="d-flex child-flex"
           cols="4"
         >
-          <CategoryItem :categoryItem="categoryItem" />
+          <CategoryItem :item="item" />
         </v-col>
       </v-row>
     </v-container>
--- frontend/src/components/main/TopCarousels.vue
+++ frontend/src/components/main/TopCarousels.vue
@@ -46,7 +46,7 @@ export default {
     }
   },
   created() {
-    getTopCarousels()
+    
   }
 }
 </script>
