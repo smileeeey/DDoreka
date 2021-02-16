c131e3d93182d5ac708c5aa6b86def70b9ab95d4
sumin416 <sumin416@naver.com>
2021-02-17 02:53:08 +0900

    Add latest product per category1 & Add v26 jar file

--- backend/Product/src/main/java/com/eureka/product/repository/ProductRepository.java
+++ backend/Product/src/main/java/com/eureka/product/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.eureka.product.dto.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
@@ -25,4 +26,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Page<Product> findByCategory1IdAndNameContains(String category1Id, String keyword, Pageable pageable);
 
     List<Product> findByStoreId(int storeId);
+
+    List<Product> findTop23ByCategory1IdOrderByRegisterDateDesc(String id);
 }
