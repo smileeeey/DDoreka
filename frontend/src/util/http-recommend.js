7254e177637d00df5d0bfe70035fd0b1f972fd71
정준영 <dkwjdiq@naver.com>
2021-04-20 14:12:29 +0900

    create Http

---
+++ frontend/src/util/http-recommend.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// axios 객체 생성
+export default axios.create({
+  baseURL: 'http://localhost:8085/springboot/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
