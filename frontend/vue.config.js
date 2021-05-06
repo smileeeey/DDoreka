8e93b64860408800da31cccd90aca8481a37b53d
sumin416 <sumin416@naver.com>
2021-05-07 04:27:38 +0900

    오타 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // 개발 서버 설정
   devServer: {
     hot: true, // 바뀐 모듈만 리로드
-    idsableHostCheck: true,
+    disableHostCheck: true,
     // 프록시 설정
     proxy: {
       // 프록시 요청을 보낼 api의 시작 부분
