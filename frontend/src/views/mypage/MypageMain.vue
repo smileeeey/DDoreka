bf8a79b6a6bded5789c3b5070ad6a7779df6a196
DESKTOP-KVCQHCD\multicampus <fkdldjs487@gmail.com>
2021-05-07 13:19:32 +0900

    Update S04P31D104-90 : Main/firebase 에러 주석 처리

---
+++ frontend/src/views/mypage/MypageMain.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="width: 100%;">
+    <v-row style="width: 100%;">
+      <v-col cols="12" style="padding-bottom: 0;">
+        <MypageAppBar />
+      </v-col>
+      <v-col cols="2" style="padding-right: 0;">
+        <MypageNav />
+      </v-col>
+      <v-col cols="10" style="padding: 2rem;">
+        <router-view />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import MypageAppBar from '../../components/mypage/MypageAppBar.vue'
+import MypageNav from '../../components/mypage/MypageNav.vue'
+export default {
+  components: { MypageNav, MypageAppBar },
+  name: 'Maypage',
+  created: function () {
+    const token = localStorage.getItem('eureka-authorization')
+    if (!token) {
+      this.$router.push({ name: 'Login' })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
