b4d9b8887688505777ec3775331d624ec7a25b4b
DESKTOP-KVCQHCD\multicampus <fkdldjs487@gmail.com>
2021-05-10 19:36:26 +0900

    Update S04P31D104-109 : 로그인 기능 actions 이동 및 api 적용

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,13 +1,24 @@
 import {request} from './index.js';
+import axios from 'axios';
 
-const authDomin = "http://localhost:8088/login";
+const authDomin = "http://k4d104.p.ssafy.io:8088/login";
 
 export const auth = {
-  //로그인
-  login(password, role, username) {
-    return request(authDomin ,"post", "add", { password, role, username });
+  // 로그인하는데 필요한 토큰값 가져오기
+  login(email, password){
+    return request(authDomin, "post", "", {username : email, password});
+  },
+
+  // 회원가입
+  loginAdd(password, role, username) {
+    return request(authDomin ,"post", "/add", { password, role, username });
   },
   update(password, role, username) {
-    return request(authDomin ,"put", "update", { password, role, username });
+    return request(authDomin ,"put", "/update", { password, role, username });
   }
+};
+
+export const setAuthInHeader = (token) => {
+  console.log("토큰:" + token);
+  axios.defaults.headers.common["eureka-authorization"] = token ? `Bearer ${token}` : null;
 };
--- frontend/src/store/modules/accountStore.js
+++ frontend/src/store/modules/accountStore.js
@@ -1,11 +1,58 @@
 // actions에 맡게 import 하세요~~~
+import { auth, setAuthInHeader} from "../../api/auth.js";
+import { user, cart } from "../../api/user.js";
 
 const accountStore = {
   namespaced: true,
-  state: {},
+  state: {
+    login: false,
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_LOGOUT(state) {
+      state.login = false;
+      state.userId = null;
+      state.wishlist = [];
+      state.name = null;
+      state.email = null;
+      state.phone = null;
+    },
+    SET_LOGIN(state, data) {
+      state.login = true;
+      state.userId = data.id;
+      state.name = data.name;
+      state.email = data.email;
+      state.phone = data.phone;
+    },
+  },
+  actions: {
+    async LOGIN({ commit, rootState}, { email, password }) {
+      console.log(email);
+      let loginData = await auth.login(email, password);
+
+      localStorage.setItem("eureka-authorization", loginData.headers["eureka-authorization"]);
+
+      setAuthInHeader(loginData.headers["eureka-authorization"]);
+
+      loginData = await user.login(email, password);
+      
+      // 추후 삭제~~!!!!
+      rootState.login = true;
+      rootState.userId = loginData.data.data.id;
+      rootState.name = loginData.data.data.name;
+      rootState.email = loginData.data.data.email;
+      rootState.phone = loginData.data.data.phone;
+
+      commit("SET_LOGIN", loginData.data.data);
+
+      let cartData = await cart.fetch(email);
+
+      //추후 삭제~~~!!!!
+      rootState.wishlist = cartData.data.data;
+
+      return;
+    },
+  },
 };
 
 export default accountStore;
