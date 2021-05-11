89729999fe3477ba70870f3e0ae4fa19828cba76
jaehyukshim <jaehyukawot@gmail.com>
2021-05-11 11:06:35 +0900

    Refactoring S04P31D104-65 Seller Server Constructor DI 재설정 및 MVC 패턴 구조 변경

--- backend/Seller/src/main/java/com/eureka/seller/service/StoreService.java
+++ backend/Seller/src/main/java/com/eureka/seller/service/StoreService.java
@@ -1,53 +1,17 @@
 package com.eureka.seller.service;
 
 import com.eureka.seller.entity.Store;
-import com.eureka.seller.repository.StoreRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class StoreService {
-    @Autowired
-    private StoreRepository repository;
-
-    public Store saveStore(Store store) {
-        return repository.save(store);
-    }
-
-    public List<Store> saveStores(List<Store> stores) {
-        return repository.saveAll(stores);
-    }
-
-    public List<Store> getStores() {
-        return repository.findAll();
-    }
-
-    public Store getStoreById(int id) {
-        return repository.findById(id).orElse(null);
-    }
-    public Store getStoreBySellerId(int sellerId) {
-        return repository.findAllBySellerIdOrderByIdDesc(sellerId).get(0);
-    }
-
-    public Store getStoreByName(String name) {
-        return repository.findByName(name);
-    }
-
-    public String deleteStore(int id) {
-        repository.deleteById(id);
-        return "store removed !!" + id;
-    }
-
+public interface StoreService {
+    Store saveStore(Store store);
+    List<Store> saveStores(List<Store> stores);
+    List<Store> getStores();
+    Store getStoreById(int id);
+    Store getStoreBySellerId(int sellerId);
+    Store getStoreByName(String name);
+    String deleteStore(int id);
     //need to add function to change seller id
-    public Store updateStore(Store store) {
-        Store existingStore = repository.findById(store.getId()).orElse(null);
-        existingStore.setName(store.getName());
-        existingStore.setPhone(store.getPhone());
-        existingStore.setMain_address(store.getMain_address());
-        existingStore.setSub_address(store.getSub_address());
-        existingStore.setZipcode(store.getZipcode());
-        return repository.save(existingStore);
-    }
+    Store updateStore(Store store);
 }
