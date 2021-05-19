1ec8e7d85d17fdba22f74719d0ca22e76c55e221
DESKTOP-KVCQHCD\multicampus <dkwjdiq@naver.com>
2021-05-20 02:19:23 +0900

    Add S04P31D104-149 : 수정된API적용/디테일페이지/상단판매정보(TopInfo)

--- frontend/src/components/itemdetail/TopInfoCard.vue
+++ frontend/src/components/itemdetail/TopInfoCard.vue
@@ -4,25 +4,25 @@
       <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
     </template>
 
-    <v-card-title class="pb-1">{{ item.name }} </v-card-title>
+    <v-card-title class="pb-1">{{ detailProductInfo.name }} </v-card-title>
     <v-card-text>
       <v-container class="pl-0">
         <v-row align="center" class="mx-0">
-          <v-rating :value="item.rating" color="warning" background-color="warning lighten-1" dense half-increments readonly size="20"></v-rating>
+          <v-rating :value="detailProductInfo.rating" color="warning" background-color="warning lighten-1" dense half-increments readonly size="20"></v-rating>
 
-          <div class="grey--text ml-4">({{ item.reviewCnt }})</div>
+          <div class="grey--text ml-4">({{ detailProductInfo.reviewCnt }})</div>
         </v-row>
       </v-container>
 
       <v-divider></v-divider>
       <div class="mt-5">
-        {{ discountRate }}%<span class="text-decoration-line-through ml-2">{{ item.options[0].price | comma }}원</span>
+        {{ discountRate }}%<span class="text-decoration-line-through ml-2">{{ detailProductInfo.options[0].price | comma }}원</span>
       </div>
-      <div class="subtitle-1 red--text text--darken-2">{{ item.options[0].discountPrice | comma }}원</div>
+      <div class="subtitle-1 red--text text--darken-2">{{ detailProductInfo.options[0].discountPrice | comma }}원</div>
       <div class="my-4 subtitle-1">무료배송</div>
       <div>
         <!-- 선택한 옵션이 object로 리턴된다. -->
-        <v-select v-model="selectOption" :items="item.options" label="옵션선택" item-text="name" solo return-object></v-select>
+        <v-select v-model="selectOption" :items="detailProductInfo.options" label="옵션선택" item-text="name" solo return-object></v-select>
         <v-select v-model="quantity" :items="numbers" label="수량" solo></v-select>
       </div>
     </v-card-text>
@@ -51,45 +51,39 @@
 import { mapState, mapActions } from 'vuex';
 export default {
   name: 'TopInfoCard',
-  props: {
-    item: Object,
-  },
   data: () => ({
     selectOption: '',
-    price: 0,
-    discountPrice: 0,
     numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     quantity: 0,
   }),
   computed: {
-    ...mapState('accountStore', ['userData','isLogin']),
+    ...mapState('accountStore', ['userData', 'isLogin']),
+    ...mapState('mainStore', ['detailProductInfo']),
     discountRate: function() {
       // 할인율 계산
-      return Math.round(((this.item.options[0].price - this.item.options[0].discountPrice) / this.item.options[0].price) * 100);
+      return Math.round(((this.detailProductInfo.options[0].price - this.detailProductInfo.options[0].discountPrice) / this.detailProductInfo.options[0].price) * 100);
     },
   },
   methods: {
-    ...mapActions('accountStore',['UPDATE_CART']),
+    ...mapActions('accountStore', ['UPDATE_CART']),
     addWishList() {
-      if(this.isLogin == false){
-        alert("로그인을 해주세요");
+      if (this.isLogin == false) {
+        alert('로그인을 해주세요');
         return;
       }
 
       this.UPDATE_CART({
-      userEmail :this.userData.email, 
-      productId : this.item.id, 
-      optionId : this.selectOption.optionId, 
-      quantity : this.quantity
-      })
-      .then((res)=>{
-        if(res) alert("상품이 장바구니에 추가되었습니다.");
-        else alert("이미 있는 상품입니다.");
+        userEmail: this.userData.email,
+        productId: this.detailProductInfo.id,
+        optionId: this.selectOption.optionId,
+        quantity: this.quantity,
+      }).then((res) => {
+        if (res) alert('상품이 장바구니에 추가되었습니다.');
+        else alert('이미 있는 상품입니다.');
       });
-
     },
     buyNow() {
-      if(this.isLogin == false) this.$router.push({ name: 'Login' });
+      if (this.isLogin == false) this.$router.push({ name: 'Login' });
       this.addWishList();
       this.$router.push({ name: 'Cart' });
     },
